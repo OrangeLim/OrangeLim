3678b500bab4d7cc44a2c02ec0bec02649d15a19
OrangeLim <xiaoxilim445@gmail.com>
2024-07-20 03:46:18 +0800

    Update RingProgressMesh.js

--- libs/RingProgressMesh.js
+++ libs/RingProgressMesh.js
@@ -108,7 +108,7 @@ class RingProgressMesh extends Mesh {
       
             // Draw text below the arc
             const textX = canvasWidth/2;
-            const textY = 480; // Position text below the arc
+            const textY = canvasHeight - (fontSize * 0.05); // Position text below the arc
       
             // Draw text on canvas
             this._context.fillText(text, textX, textY);
